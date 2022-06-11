a07bc0905f829df289be981182f95fa689e8fd60
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-06-11 13:08:38 -0700

    Time: 737 ms (16.66%), Space: 17.8 MB (93.47%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,24 +1,17 @@
 class MinStack:
 
     def __init__(self):
-      self._stack = []
+          self._stack = []
 
     def push(self, val: int) -> None:
-      if self._stack:
-        if val < self._stack[-1][1]:
-          currMin = val
-        else:
-          currMin = self._stack[-1][1]
-      else:
-        currMin = val
-      self._stack.append([val, currMin])
-      
+          self._stack.append(val)
 
     def pop(self) -> None:
-        self._stack.pop()
+            self._stack.pop()
 
     def top(self) -> int:
-        return self._stack[-1][0]
+            return self._stack[-1] 
 
     def getMin(self) -> int:
-      return self._stack[-1][1]
+            return sorted(self._stack)[0]
+        
