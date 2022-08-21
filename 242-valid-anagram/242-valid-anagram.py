25be0b310e21f9aaf92e63b8dec3dd8d71148368
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-08-21 13:16:05 -0700

    Time: 97 ms (22.88%), Space: 15.2 MB (11.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,28 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        '''
-       input of two strings s and t 
-       if t is an anagram of s (if can rearrange letters to form same word, must use all letters once)
-       output true
-       if not 
-       output false
         
-        if s and t aren't same length, automatically return false
-        if same length, add chars of each string to an new string after sorting and compare the resulting strings
-        if exactly equal, return true
-        if not exactly equal, return false
-        '''
-        
-        if len(s) != len(t):
-            return False
-        
-        new_s = ''.join(sorted(s))
-        new_t = ''.join(sorted(t))
-       
-        if new_s == new_t:
+        #input two strings s and t
+        #if t is an anagram of s
+        #return true
+        #else, return false    
+
+        if sorted(s) == sorted(t):
             return True
-        else:
-            return False
-            
-        
+        return False
         
