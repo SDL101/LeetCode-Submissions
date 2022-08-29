e552781c13c0edb49d0cc826d37e6dc1229329ec
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-08-28 20:00:37 -0700

    Time: 69 ms (87.96%), Space: 13.8 MB (59.57%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,8 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        isPalindrome = False
+        #compare 1st and last indecy values
+        #move left pointer up 1 and right pointer down one
+        #continue this while left pointer is < len(x) // 2
+        #return false if any comparisons don't match
+        #return true if loop completes and all comparisons are same
+        
+        lp = 0 
+        rp = -1
         str_x = str(x)
-        if str_x[::-1] == str_x:
-            isPalindrome = True
-        return isPalindrome
-     
+        stop_point = len(str_x) // 2
+        while lp < stop_point:
+            if str_x[lp] != str_x[rp]:
+                return False
+            else:
+                lp += 1
+                rp -=1
+        return True
