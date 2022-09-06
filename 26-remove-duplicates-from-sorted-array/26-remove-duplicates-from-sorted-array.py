217432363df464718868df9752fd840eab35d946
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-06 16:25:08 -0700

    Time: 164 ms (38.60%), Space: 15.7 MB (18.77%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,6 +1,5 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
         l, r = 0, 1
         
         while r < len(nums):
@@ -9,6 +8,5 @@ class Solution:
             else:
                 nums[l+1] = nums[r]
                 l += 1
-                r = r + 1
-                
-        return l + 1
+                r += 1
+        return l + 1   
