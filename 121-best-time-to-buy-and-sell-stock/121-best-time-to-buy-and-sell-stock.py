d6c24b9369c10b43ed6c39fa8d0b02fc7f651183
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-07 15:31:49 -0700

    Time: 4030 ms (5.01%), Space: 25 MB (38.37%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,30 +1,36 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-      l,r = 0,1
-      maxProf = 0
-      num_els = len(prices)
+        
+        #loop through the array prices
+        #2 pointers - l starts at 0, r starts at 1
+        #check the difference between r and l 
+        #if the diff is greater than the max profit
+        #update the max profit and move the right pointer
+        #when right pointer reaches the end of the array
+        #move the left pointer to the right one index
+        #move the right pointer to one index position to the right of left
+        
+#         l, r = 0, 1
+#         max_prof = 0
+#         # for each in range(len(prices)):
+#         while r < len(prices):
+#             profit = prices[r] - prices[l]
+#             max_prof = max(max_prof, profit)
+#             r += 1
+#         l += 1
+#         r = l + 1
 
-      while r < num_els:
-        if prices[l] < prices[r]:
-          profit = prices[r] - prices[l]
-          maxProf = max(maxProf, profit)
-        else: 
-            l = r
-        r += 1
-      return maxProf
+#         return max_prof
 
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-      l,r = 0,1
-      maxProf = 0
-
-      while r < len(prices):
-        if prices[l] < prices[r]:
-          profit = prices[r] - prices[l]
-          maxProf = max(maxProf, profit)
-        else: 
-            l = r
-        r += 1
-      return maxProf
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+    
