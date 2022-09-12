567432afa7f4ef0f26917cad52a751477b906b7a
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-11 18:46:28 -0700

    Time: 71 ms (88.15%), Space: 15.1 MB (49.76%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,24 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        valindex_map = {}
         
-#         nums = [2,7,11,15]
-#         target = 22
-
-#create empty hashmap - keys as nums and values as indeces 
-#loop through my nums array
-#take my target val and subtract num from each iteration of the loop 
-#check if resulting value is in my hashmap 
-#if not, add the num
-#if it is, return the value index and the num iteration index
-
-        hashy_map = {}
-        index_array = []
         for index, num in enumerate(nums):
-            diff = target - num
-            if diff in hashy_map:
-                index_array.append(index)
-                index_array.append(hashy_map[diff])
+            value = target - num
+            if value in valindex_map:
+                return valindex_map[value], index
             else:
-                hashy_map[num] = index
-                
-        return index_array
+                valindex_map[num] = index
+            
+            
