6f040dabcf024a7ad8bc6d1373f9504c3ab0451f
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-13 17:56:19 -0700

    Time: 124 ms (47.06%), Space: 15.2 MB (49.81%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        valIndexMap = {}
+        visitedMap = {}
         
         for index, num in enumerate(nums):
-            value = target - num
-            if value in valIndexMap:
-                return valIndexMap[value], index
+            val = target - num
+            if val in visitedMap:
+                return index, visitedMap[val]
             else:
-                valIndexMap[num] = index 
-                
+                visitedMap[num] = index
+        
