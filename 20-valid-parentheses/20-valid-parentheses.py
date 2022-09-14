c704c7a79f3710e6e9cfe6c69d2f06392af0ab2e
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-13 18:26:32 -0700

    Time: 28 ms (97.59%), Space: 13.8 MB (72.22%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,40 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #is input s = "({)}" considered valid?
+        closeToOpen = {")":"(",
+                       "}":"{",
+                       "]":"["}
         
-        #create dict with closing bracket as key, and opening bracket as the value
-        #create empty stack
-        #loop through s
-        #if char is an opening bracket, add to stack 
-        #if char is a closing bracket, check and see if it's corresponding dict value is on the top of stack
-        #if true then pop it off the stack
-        #if false, return false
-        #once looping is complete, return true if stack is empty
-        #return false if stack contains any elements
-
-        brackMap = {")":"(", 
-                    "]":"[",
-                    "}":"{"}
+        brack_stack = []
         
-        brackStack = []
-        
-        # s = "()[]{}"
-        #Output: true
-        
+ 
         # s = "(]"
-        #Output: false
-        
         for char in s:
-            if char not in brackMap.keys():
-                brackStack.append(char)
-            elif char in brackMap.keys():
-                if brackStack and brackStack[-1] == brackMap[char]:
-                    brackStack.pop()
+            if char in closeToOpen.keys() and len(brack_stack) == 0:
+                return False
+            elif char in closeToOpen.keys():
+                if brack_stack[-1] == closeToOpen[char]:
+                    brack_stack.pop()
                 else:
                     return False
+                    
+            elif char in closeToOpen.values():
+                brack_stack.append(char)
             
-        if brackStack == []:
-            return True
-        else:
+        if len(brack_stack) > 0:
             return False
-        
+        else:
+            return True
