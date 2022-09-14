995e660519d2ab9a404b2edd4c5c459aa6416831
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-13 21:09:03 -0700

    Time: 2473 ms (10.57%), Space: 25 MB (85.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,35 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
         
-        #loop through the array prices
-        #2 pointers - l starts at 0, r starts at 1
-        #check the difference between r and l 
-        #if the diff is greater than the max profit
-        #update the max profit and move the right pointer
-        #when right pointer reaches the end of the array
-        #move the left pointer to the right one index
-        #move the right pointer to one index position to the right of left
-        
-#         l, r = 0, 1
-#         max_prof = 0
-#         # for each in range(len(prices)):
-#         while r < len(prices):
-#             profit = prices[r] - prices[l]
-#             max_prof = max(max_prof, profit)
-#             r += 1
-#         l += 1
-#         r = l + 1
+        maxProfit = 0
 
-#         return max_prof
-
-      l,r = 0,1
-      maxProf = 0
-
-      while r < len(prices):
-        if prices[l] < prices[r]:
-          profit = prices[r] - prices[l]
-          maxProf = max(maxProf, profit)
-        else: 
-            l = r
-        r += 1
-      return maxProf
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(profit, maxProfit)
+            else:
+                l = r
+            r += 1     
+        return maxProfit
