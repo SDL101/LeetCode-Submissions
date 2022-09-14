b9e34cdd7c71dc13837eaa21abb59f5989f41197
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-13 18:49:05 -0700

    Time: 75 ms (18.59%), Space: 13.9 MB (32.50%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,27 +5,28 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        l1, l2 = list1, list2
         dummy = ListNode()
-        tail = dummy 
+        orig = dummy
+        cur = dummy
         
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            elif l2.val <= l1.val:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next 
             
-            else:
-                tail.next = list2
-                list2 = list2.next
+        while l1 and not l2:
+            cur.next = l1
+            l1 = l1.next 
+            cur = cur.next 
+
+        while l2 and not l1:
+            cur.next = l2
+            l2 = l2.next 
+            cur = cur.next 
                 
-            tail = tail.next
-        
-        if list1:
-            tail.next = list1
-            list1 = list1.next
-            
-        elif list2:
-            tail.next = list2
-            list1 = list2.next
-            
-        return dummy.next
+        return orig.next 
