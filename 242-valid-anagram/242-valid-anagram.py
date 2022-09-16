dd92b527755054b7fc812940735615c2eb780169
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-16 13:19:27 -0700

    Time: 118 ms (12.05%), Space: 14.6 MB (35.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,12 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         
-        #input two strings s and t
-        #if t is an anagram of s
-        #return true
-        #else, return false    
-
-        if sorted(s) == sorted(t):
+        sFreq = {}
+        tFreq = {}
+        for char in s:
+            if char in sFreq.keys():
+                sFreq[char] += 1
+            else:
+                sFreq[char] = 1
+        for char in t:
+            if char in tFreq.keys():
+                tFreq[char] += 1
+            else:
+                tFreq[char] = 1
+                
+        if sFreq == tFreq:
             return True
-        return False
-        
+        else:
+            return False
+                
+            
