5012da41a42fc503b203e560b2b2394b805a39cc
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-16 13:23:00 -0700

    Time: 90 ms (36.28%), Space: 15.1 MB (11.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,22 +3,8 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        sFreq = {}
-        tFreq = {}
-        for char in s:
-            if char in sFreq.keys():
-                sFreq[char] += 1
-            else:
-                sFreq[char] = 1
-        for char in t:
-            if char in tFreq.keys():
-                tFreq[char] += 1
-            else:
-                tFreq[char] = 1
-                
-        if sFreq == tFreq:
+        if sorted(s) == sorted(t):
             return True
-        else:
-            return False
+        return False
                 
             
