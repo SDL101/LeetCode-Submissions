6125b3e390f145eb947cafef41945ec5016a0bf4
SDL101 <81109215+SDL101@users.noreply.github.com>
2022-09-19 22:24:11 -0700

    Time: 148 ms (16.00%), Space: 14.1 MB (53.01%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # ransomNote = "aabb"
-        # magazine = "ab"
-        new_mag_array = []
-        for mag_char in magazine:
-            new_mag_array.append(mag_char)
-            #new_mag_array = [a, b]
-        for note_char in ransomNote:
-            if note_char in new_mag_array:
-                new_mag_array.remove(note_char)
+        magMap = {}
+        for char in magazine:
+            if char in magMap:
+                magMap[char] += 1
             else:
-                return False
-        return True
+                magMap[char] = 1
+            
         
+        for c in ransomNote:
+            if c not in magMap.keys() or magMap[c] == 0:
+                return False
+            magMap[c] -= 1
+        return True
+            
