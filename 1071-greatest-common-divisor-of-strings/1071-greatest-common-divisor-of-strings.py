52986e428169115a9faa2ecf02f44f347e2e6939
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2024-05-19 17:13:25 -0700

    Time: 30 ms (90.04%), Space: 16.5 MB (96.58%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,10 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        
         if str1 + str2 != str2 + str1:
             return ""
         
-        max_length = gcd(len(str1), len(str2))
-        return str2[:max_length]
-    
-    
+        gcd_len = gcd(len(str1), len(str2))
+        return str1[:gcd_len]
