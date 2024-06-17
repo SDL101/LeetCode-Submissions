3ddc19230bb6c6faaa6259fd349641eb9e8f6719
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2024-06-16 20:34:04 -0700

    Time: 42 ms (27.72%), Space: 16.4 MB (76.25%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -2,11 +2,13 @@ class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         altList = [0]
         count = 0
+        maxGain = 0
         
         for netgain in gain:
-            count += netgain 
+            count += netgain
+            maxGain = max(maxGain, count)
             altList.append(count)
             
-        return max(altList)
+        return maxGain
             
             
