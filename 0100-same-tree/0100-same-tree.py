037039b9cefb034abf491155fe92300ed96acc01
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2024-07-21 13:11:37 -0500

    Time: 30 ms (87.10%), Space: 16.6 MB (26.04%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,11 +6,18 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        #both trees were empty or we successfully recursed to the base case - return true
         if not p and not q:
             return True
+        
+        #found node existence diff - return false
         if not p or not q:
             return False
-        if p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
+        
+        #found node value diff between trees during recursion - return false
+        if p.val != q.val:
             return False
+        
+        #return the res of calling both left and right recursively
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
