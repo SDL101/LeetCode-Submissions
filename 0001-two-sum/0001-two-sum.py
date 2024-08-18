cdfdded3b5ac2259ca35c28a343e34bdb83744d6
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2024-08-18 18:55:10 -0500

    Time: 57 ms (73.12%), Space: 17.7 MB (28.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,7 @@
-from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        numMap = defaultdict(int)
+        iMap = collections.defaultdict(int)
         for i, num in enumerate(nums):
-            if (target - num) in numMap.keys():
-                return [i, numMap[target-num]]
-            numMap[num] = i
+            if (target - num) in iMap:
+                return [i, iMap[target - num]]
+            iMap[num] = i
