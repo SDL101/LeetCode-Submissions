70d50c4370e125f7f6344bfe59f874d6ff5b6c9b
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2025-05-27 19:09:43 -0700

    Time: 0 ms (100%), Space: 19 MB (34.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        iMap = collections.defaultdict(int)
+        visitedIdxMap = {}
         for i, num in enumerate(nums):
-            if (target - num) in iMap:
-                return [i, iMap[target - num]]
-            iMap[num] = i
+            diff = target - num
+            if diff in visitedIdxMap:
+                return [visitedIdxMap[diff], i]
+            visitedIdxMap[num] = i
