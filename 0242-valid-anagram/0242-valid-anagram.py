1645db57521d2396b7003c75582e474b1311c188
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2025-05-27 18:52:55 -0700

    Time: 10 ms (75.1%), Space: 18 MB (48.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sMap, tMap = Counter(s), Counter(t)
-        return sMap == tMap
+        if len(s) != len(t):
+            return False
+        SbucketsArr, TbucketsArr= [0] * 26, [0] * 26
+        
+        for i in range(len(s)):
+            SbucketIndex = ord(s[i]) - 97
+            SbucketsArr[SbucketIndex] = SbucketsArr[SbucketIndex] + 1
+
+            TbucketIndex = ord(t[i]) - 97
+            TbucketsArr[TbucketIndex] = TbucketsArr[TbucketIndex] + 1
+        if SbucketsArr == TbucketsArr:
+            return True
+        return False
+
+        
