4b9ee3aac6a9b256d0d88db13be1bc63af217804
Scott Lindsay <81109215+SDL101@users.noreply.github.com>
2025-05-28 20:15:18 -0700

    Time: 30 ms (10.71%), Space: 21.4 MB (40.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        gramDict = collections.defaultdict(list)
-        for string in strs:
-            sortString = tuple(sorted(string))
-            gramDict[sortString].append(string)
-        
+        bucketMap = {}
+
+        for word in strs:
+            currBucket = [0] * 26
+            for char in word:
+                mappedIndex = (ord(char) - 97)
+                currBucket[mappedIndex] = currBucket[mappedIndex] + 1
+            bucketStr = str(currBucket)
+            if bucketStr in bucketMap:
+                bucketMap[bucketStr].append(word)
+            else:
+                bucketMap[bucketStr] = [word]
         res = []
-        for valList in gramDict.values():
-            res.append(valList)
+        for wordList in bucketMap.values():
+            res.append(wordList)
         return res
