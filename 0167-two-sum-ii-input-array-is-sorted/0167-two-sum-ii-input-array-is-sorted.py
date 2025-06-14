1e9176f61bc3e80843621d4488ae6a61a2e41146
Scott Lindsay <scottdaniellindsay@gmail.com>
2025-06-14 14:46:26 -0500

    Time: 4 ms (48.13%), Space: 18.7 MB (9.51%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,27 +3,24 @@ class Solution:
         # 0. Problem notes - arr sorted in non-dec ord, convert to 1-indexed arr for output, exactly one solution
         #                  - must use const extra space
 
-        # 1. The sorting makes me think we can use 2 pts to find the two indices
-
-
+        # 1. Use 2 ptrs(start and end of arr)
         l, r = 0, len(numbers) - 1
+        # 2. While we still have unchecked unique idx pairs and no solutions
         while l < r:
             currSum = numbers[l] + numbers[r]
+            # 3. Success case - found two idx's that add to target, inc both for 1-idx output and return
             if currSum == target:
                 l += 1
                 r += 1
                 indexArr = [l, r]
                 return indexArr
+            # 4. Sum too large case - need to move right ptr down toward smaller numbers (since arr sorted non dec ord)
             elif currSum > target:
                 r -= 1
+            # 5. Sum too small case - need to move left ptr up toward larger numbers (since arr sorted non dec ord)
             elif currSum < target:
                 l += 1
 
+# Time Complexity: O(n) aka Linear - since in the worst case, our iteration of ptrs scales roughly linearly w/ input arr len
 
-
-
-
-
-# Time Complexity:
-
-# Space Complexity: 
+# Space Complexity: O(1) aka Constant - since we are only storing the indexArr of len = 2 and the currSum 
