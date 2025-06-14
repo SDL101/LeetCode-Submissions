b1f451dcdf1abae35302b0fe4816e54c0eb30592
Scott Lindsay <scottdaniellindsay@gmail.com>
2025-06-14 14:36:07 -0500

    Time: 0 ms (100%), Space: 18.5 MB (90.11%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,17 +1,29 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) -1
+        # 0. Problem notes - arr sorted in non-dec ord, convert to 1-indexed arr for output, exactly one solution
+        #                  - must use const extra space
 
+        # 1. The sorting makes me think we can use 2 pts to find the two indices
+
+
+        l, r = 0, len(numbers) - 1
         while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            else:
+            currSum = numbers[l] + numbers[r]
+            if currSum == target:
                 l += 1
-        
-            
-            
+                r += 1
+                indexArr = [l, r]
+                return indexArr
+            elif currSum > target:
+                r -= 1
+            elif currSum < target:
+                l += 1
 
 
-        
+
+
+
+
+# Time Complexity:
+
+# Space Complexity: 
