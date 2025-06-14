0646857d02bce97e9ae1dc8387d2cb72e9b312a4
Scott Lindsay <scottdaniellindsay@gmail.com>
2025-06-14 14:14:41 -0500

    Time: 5 ms (37.12%), Space: 19.4 MB (5.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        visitedIdxMap = {}
+        # 0. Algo Notes - need linear (Big O of n) solution, one solution exactly, nums.length >= 2 
+
+        # 1. Create a hashmap of: {num: index} for each num visited
+        numMap = {}
+        # 2. Loop over each num in nums
         for i, num in enumerate(nums):
+        # 3. Check if (target - num) exists in hashmap keys
             diff = target - num
-            if diff in visitedIdxMap:
-                return [visitedIdxMap[diff], i]
-            visitedIdxMap[num] = i
+            # 4. If the diff (the val we're searching for) isn't in the numMap, add it as {num: index}
+            if diff not in numMap:
+                numMap[num] = [i]
+            # 5. Otherwise, the value we need (which is diff) has been visited, 
+            # we need to ret: [curr idx in loop, corresponding value of diff's key in map which is its idx]
+            else:
+                numMap[diff].append(i)
+                return numMap[diff]
+
