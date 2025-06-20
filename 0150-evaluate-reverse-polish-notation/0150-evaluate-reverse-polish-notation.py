7151d5800d659102e467a92013b7f7b5b8db8f6d
Scott Lindsay <scottdaniellindsay@gmail.com>
2025-06-20 15:54:30 -0500

    Time: 3 ms (64.94%), Space: 19.1 MB (87.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,7 +1,5 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-
-        # dont forget to round down on division - use floor() 
         stack = []
 
         for token in tokens:
@@ -12,7 +10,7 @@ class Solution:
             elif token == "/":
                 second = stack.pop()
                 first = stack.pop()
-                stack.append(int(first/second))
+                stack.append(int(first/second)) #round any dec vals toward 0. (can use int() or math.trunc(), not same as round down since neg/pos)
             elif token == "+":
                 second = stack.pop()
                 first = stack.pop()
@@ -22,6 +20,9 @@ class Solution:
                 first = stack.pop()
                 stack.append(first - second)
             else:
-                stack.append(int(token)) 
+                stack.append(int(token))  #make sure to type cast to int and don't add the str version to stack
 
         return stack[0]
+
+    # Time Complexity: O(n) - looping through the whole tokens arr
+    # Space Complexity: O(n) - stack scales linearly with the size of the tokens arr
