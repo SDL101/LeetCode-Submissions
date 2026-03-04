0bfa149814de7cb42a6d06c7d23cecf19a6345a2
Scott Lindsay <scottdaniellindsay@gmail.com>
2026-03-03 18:34:22 -0800

    Time: 0 ms (100%), Space: 19.2 MB (84.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -7,12 +7,6 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         # create dummy head, dummy head ptr, and 2 ptrs for sliding, start right pointer over n times, then slide both ptrs together until right.next is None. remove by setting left.next = right. return dummy head.next
 
-        # head = [X, 1,  2,  3,  4,  5], n = 2
-                            #  ^       ^
-        # head = [X, 1], n = 1
-            
-                #  ^  ^
-
         dummyHead = ListNode()
         dummyHead.next = head
         l, r = dummyHead, dummyHead
